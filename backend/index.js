5f25e7d1545aa8666d25bead348a2f34936b05d3
Anurag-Bachheti <anuragbachheti1999@gmail.com>
2026-01-05 12:42:47 +0530

    feat: controller and db file seperated

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.js';
+import {connectDB} from './db.js'
 
 dotenv.config();
 const app = express();
@@ -20,13 +20,6 @@ app.get('/', (req,res)=> {
     res.send("Backend is running...")
 })
 
-// MongoDB connection
-// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(()=> console.log("MongoDB connected"))
-//     .catch(error=> console.log(error));
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
-
-app.listen(PORT, ()=> console.log(`Server running in PORT ${PORT}`));
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+});
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,52 +1,9 @@
-import express from 'express';
-import User from '../models/User.js';
-import jwt from 'jsonwebtoken';
+import express from "express";
+import { register, login } from "../controllers/authController.js";
 
 const router = express.Router();
 
-//REGISTER
-router.post('/register', async(req,res)=> {
-    try{
-        const {name, email, password} = req.body;
-
-        // check if user exists
-        const existingUser = await User.findOne({email});
-        if(existingUser) return res.status(400).json({ message: "User already exists" });
-
-        // Hash Password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await brcypt.hash(password, salt);
-
-        // Create User
-        const newUser = new User({name, email, password: {hashedPassword}});
-        await newUser.save();
-
-        res.status(201).json({message: "User created successfully"});
-    }catch(err){
-        res.status(500).json({message: err.message})
-    }
-});
-
-// LOGIN
-router.post('/login', async(req,res)=> {
-    try{
-        const { email, password } = req.body;
-
-        // check if user exists
-        const user = await User.findOne({email});
-        if(!user) return res.status(400).json({ message: "Invalid Credentials" });
-
-        // compare password
-        const ifMatch = await bcrypt.compare(password, user.password);
-        if(!ifMatch) return res.status(400).json({ message: "Invalid Credentials" });
-
-        // create JWT
-        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: '1hr'});
-
-        res.json({token, user: {id: user_id, name: user.name, email: user.email} })
-    }catch(err){
-        res.status(500).json({message: err.message})
-    }
-})
+router.post("/register", register);
+router.post("/login", login);
 
 export default router;
